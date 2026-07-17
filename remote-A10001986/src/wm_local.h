7901ac923802eb7d355f8b97abc03f4224938684
A10001986 <76924199+realA10001986@users.noreply.github.com>
2026-07-17 10:25:56 +0200

    WM patches for 1.25

---
+++ remote-A10001986/src/wm_local.h
@@ -0,0 +1,49 @@
+/**
+ * wm_local.h - REMOTE
+ *
+ * Based on:
+ * WiFiManager, a library for the ESP32/Arduino platform
+ * Creator tzapu (tablatronix)
+ * Version 2.0.15
+ * License MIT
+ *
+ * Adapted by Thomas Winischhofer (A10001986)
+ */
+
+#ifndef wm_local_h
+#define wm_local_h
+
+#include "../../remote_global.h"
+
+//#define _A10001986_DBG
+//#define _A10001986_V_DBG
+
+#define WM_MDNS
+
+#define WM_PARAM2
+
+#ifdef WM_PARAM2
+#define WM_PARAM2_CAPTION "HA/MQTT Settings"
+#define WM_PARAM2_TITLE "HA/MQTT Settings"
+#endif
+
+#ifdef HAVE_CRSF
+#define WM_PARAM3
+#define WM_PARAM3_CAPTION "ELSR/CRSF Settings"
+#define WM_PARAM3_TITLE "ELSR/CRSF Settings"
+#define WM_DYNPARM3
+#endif
+
+#define WM_UPLOAD
+
+#define WM_CCM
+
+// #define WM_AP_STATIC_IP
+// #define WM_APCALLBACK
+// #define WM_PRECONNECTCB
+// #define WM_PRESAVECB
+// #define WM_EVENTCB
+// #define WM_ADDLGETTERS
+// #define WM_ADDLSETTERS
+
+#endif
